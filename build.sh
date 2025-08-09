edd8603067b158338c9e3f538516797e0145ff92
Murali Indukuri <indukurimkv@gmail.com>
2025-08-09 09:16:59 -0400

    Fix issues with build system

--- build.sh
+++ build.sh
@@ -6,22 +6,6 @@ cdmk (){
 }
 
 workdir=$(pwd)
-mkdir -p $workdir/lib
-
-echo "Creating build directory for clip"
-cdmk ./clip/build
-
-clip_params=""
-if (( "_$1" == "_release" )); then
-  clip_params="$clip_params -DCMAKE_BUILD_TYPE=Release"
-else
-  clip_params="$clip_params -DCMAKE_BUILD_TYPE=Debug"
-fi
-
-echo "Buidling libclip"
-cmake .. $clip_params
-make
-cp ./libclip.a $workdir/lib/
 
 cdmk "$workdir/build"
 
--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include "clip.h"
 
 int main(){
+  clip::set_text("Hello World! I'm Clippy, a super simple clipboard manager!");
+  return 0;
 }
