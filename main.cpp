22b5a8d7346e475f80ad8ecbe6635805a23e1829
Murali Indukuri <indukurimkv@gmail.com>
2025-08-09 11:46:41 -0400

    Basic testing with clip

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
+#include <functional>
 #include <iostream>
 #include "clip.h"
+#include <string>
+#include <vector>
+#include <sys/types.h>
 
 int main(){
-  clip::set_text("Hello World! I'm Clippy, a super simple clipboard manager!");
+  std::hash<std::string> hasher;
+  std::vector<std::string> copy_hist;
+  std::string temp;
+  u_int32_t temp_hash;
+  u_int32_t last_hash = 0;
+  while(true){
+    // Check for new things in clipboard and record if true
+    clip::get_text(temp);
+    temp_hash = hasher(temp);
+    if(last_hash != temp_hash){
+      copy_hist.push_back(temp);
+    }
+    last_hash = temp_hash;
+
+    //clear console
+    std::cout << "\033[2J\033[1;1H";
+    std::cout << copy_hist.size() << std::endl;
+     for(int i = 0; i < copy_hist.size(); i++){
+       std::cout << copy_hist.at(i) << '\n';
+     }
+//    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    temp.clear();
+  }
   return 0;
 }
