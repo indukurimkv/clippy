7c960462d1df98165eaecda0d84053c8955039b8
Murali Indukuri <indukurimkv@gmail.com>
2025-08-09 12:58:40 -0400

    Prototype clippy daemon

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <functional>
 #include <iostream>
 #include "clip.h"
+#include <list>
 #include <string>
-#include <vector>
 #include <sys/types.h>
+#include <thread>
+#include <chrono>
+
 
 int main(){
   std::hash<std::string> hasher;
-  std::vector<std::string> copy_hist;
+  std::list<std::string> copy_hist;
   std::string temp;
   u_int32_t temp_hash;
   u_int32_t last_hash = 0;
@@ -16,17 +19,22 @@ int main(){
     clip::get_text(temp);
     temp_hash = hasher(temp);
     if(last_hash != temp_hash){
-      copy_hist.push_back(temp);
+      copy_hist.remove_if([temp](std::string e){return e == temp;});
+      copy_hist.insert(copy_hist.begin(), temp);
     }
     last_hash = temp_hash;
 
     //clear console
     std::cout << "\033[2J\033[1;1H";
     std::cout << copy_hist.size() << std::endl;
-     for(int i = 0; i < copy_hist.size(); i++){
-       std::cout << copy_hist.at(i) << '\n';
+    for(
+      std::list<std::string>::iterator it = copy_hist.begin();
+      it != copy_hist.end(); it++
+    ){
+        std::cout << *it << std::endl;
      }
-//    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+//    IncrementIndeces(copy_hist);
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));
     temp.clear();
   }
   return 0;
